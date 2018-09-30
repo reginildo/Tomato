9ac70d068729806c4da74a2250a03270c37748fd
Reginildo Sousa <reginildosousa01@gmail.com>
2018-09-30 12:54:59 -0300

    Refatorando código...

--- src/io/github/reginildo/tomato/TomatoTimerTask.java
+++ src/io/github/reginildo/tomato/TomatoTimerTask.java
@@ -3,8 +3,15 @@ package io.github.reginildo.tomato;
 import java.util.TimerTask;
 
 public class TomatoTimerTask extends TimerTask {
+    /**
+     * criar objeto timertask e controlar a execução com uma variavel a ela
+     *
+     * */
+
+
     @Override
     public void run() {
+
 
     }
 }
